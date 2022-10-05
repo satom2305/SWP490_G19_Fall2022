016e748693a7725316f289646290afd26802908e
satom2305 <sangnq.se@gmail.com>
2022-10-06 00:42:02 +0700

    update_main

--- SWP490_TW/SWP490_TW/src/main/java/com/capstone/project/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.project.repository;
-
-import com.capstone.project.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
