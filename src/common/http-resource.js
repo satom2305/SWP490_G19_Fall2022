c0b06604c72b35068d2fd56de50caa416e325645
Minhtq30 <minhtran7420@gmail.com>
2022-12-01 01:29:53 +0700

    Revert "change guest role"
    This reverts commit 24db9e4232ec60ebc207cea2f77a52ef7c96ec28.

--- src/common/http-resource.js
+++ src/common/http-resource.js
@@ -16,7 +16,7 @@ const instance = axios.create({
 
 const requestHandler = request => {
   EventBus.$emit("send-progress", true)
-  if (verifyAccountRole()) request.headers["Authorization"] = 'Bearer ' + StorageService.get('Token')
+  if (verifyAccountRole) request.headers["Authorization"] = 'Bearer ' + StorageService.get('Token')
   if (isHandlerEnable(request)) {
     // console.log("Request Interceptor", request);
   }
