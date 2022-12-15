374fb7e66d456b4aec96f15b0a0eeeb9a24064e5
tran minh <minhtqhe141201@fpt.edu.vn>
2022-12-15 18:02:58 +0700

    config access control origin

--- vue.config.js
+++ vue.config.js
@@ -52,7 +52,7 @@ module.exports = {
     https: false,
     disableHostCheck: true,
     headers: {
-      'Access-Control-Allow-Origin': 'https://iam-admin.ghtk.local:8082',
+      'Access-Control-Allow-Origin': 'https://www.chatbottw.com/',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
       'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
     }
